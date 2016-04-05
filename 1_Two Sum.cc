5d9b236239c41258122fbf5d8352604ce27bb0b4
xf-leo <15251765089@163.COM>
2016-04-05 20:09:01 +0800

    Update 1_Two Sum.cc

--- 1_Two Sum.cc
+++ 1_Two Sum.cc
@@ -1,1 +1,23 @@
-c++
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j;
+        vector<int> indices;
+        for(i=0;i<nums.size();++i)
+        {
+          vector<int>::const_iterator it = find(nums.begin(),nums.end(),target-nums[i]);
+          if (it != nums.end())
+          {
+            indices.push_back(i);
+            indices.push_back(it-nums.begin());
+          }
+        }
+        
+        return indices;
+    }
+};
